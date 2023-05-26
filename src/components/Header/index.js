6b948b1ce7197559e3de35d1e2f5df57358cbe0d
prudhvi <prudhvikarimeti18@gmail.com>
2023-05-26 11:03:32 +0000

    tastykitchenspractice

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -56,7 +56,70 @@ const Header = props => {
           </Link>
           <h1 className="icon-heading">Tasty Kitchens</h1>
         </div>
+
+        <ul className="nav-menu">
+          <Link to="/" className="nav-link">
+            <li className="nav-menu-item" style={{color: getColor('/')}}>
+              Home
+            </li>
+          </Link>
+          <Link to="/cart" className="nav-link">
+            <li className="nav-menu-item" style={{color: getColor('/cart')}}>
+              Cart
+              {renderCartItemsCount()}
+            </li>
+          </Link>
+          <li>
+            <button
+              type="button"
+              className="logout-desktop-btn"
+              onClick={onClickLogout}
+            >
+              Logout
+            </button>
+          </li>
+        </ul>
+
+        <Popup
+          trigger={
+            <button type="button" className="hamburger-btn">
+              <GiHamburgerMenu size={25} className="hamburger" />
+            </button>
+          }
+        >
+          {close => (
+            <div className="modal-container">
+              <div className="nav-link-container">
+                <Link to="/" className="nav-link">
+                  <p className="nav-menu-item" style={{color: getColor('/')}}>
+                    Home
+                  </p>
+                </Link>
+                <Link to="/cart" className="nav-link">
+                  <p
+                    className="nav-menu-item"
+                    style={{color: getColor('/cart')}}
+                  >
+                    Cart
+                    {renderCartItemsCount()}
+                  </p>
+                </Link>
+                <button
+                  type="button"
+                  className="logout-desktop-btn"
+                  onClick={onClickLogout}
+                >
+                  Logout
+                </button>
+              </div>
+              <button type="button" className="close-btn">
+                <AiOutlineCloseCircle size={18} onClick={() => close()} />
+              </button>
+            </div>
+          )}
+        </Popup>
       </div>
     </nav>
   )
 }
+export default withRouter(Header)
